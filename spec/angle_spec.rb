3e14abbf453981b469e1a4f897a61e938c743284
mesmith454 <mesmith454@gmail.com>
2021-01-26 10:19:19 -0800

    commit tree 041edca4bc7294b89e99a782227c09194a2dcccd

--- spec/angle_spec.rb
+++ spec/angle_spec.rb
@@ -1,24 +1,16 @@
 describe "#clock_angle(time)" do
   it 'returns the correct angle between the clock hands representing 9:00' do
-    expect(clock_angle("9:00")).to eq(270)
+    expect(clock_angle("9:00")).to eq(90)
   end
-
   it 'returns the correct angle between the clock hands representing 12:00' do
     expect(clock_angle("12:00")).to eq(0)
   end
-
   it 'returns the correct angle between the clock hands representing 6:00' do
     expect(clock_angle("6:00")).to eq(180)
   end
-
   it 'returns the correct angle between the clock hands representing 3:15' do
     expect(clock_angle("3:15")).to eq(7.5)
   end
-
-  it 'returns the correct angle between the clock hands representing 3:20' do
-    expect(clock_angle("3:20")).to eq(340)
-  end
-
   it 'returns the correct angle between the clock hands representing 8:30' do
     expect(clock_angle("8:30")).to eq(75)
   end
