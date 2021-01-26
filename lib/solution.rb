8c1d1e57e201211c2eccf50dcf3e08115addf662
mesmith454 <mesmith454@gmail.com>
2021-01-26 10:21:10 -0800

    done

--- lib/solution.rb
+++ lib/solution.rb
@@ -1,3 +1,22 @@
 def clock_angle(time)
-    # code your solution here
+   hours = time.split(":")[0].to_i
+   minutes = time.split(":")[1].to_i
+    # if hour = 12
+    #     hour_angle = 0
+    # end
+
+    hour_hand_degrees_per_minute = 0.5
+
+    hour_angle = (hours * (360 / 12)) + (minutes * hour_hand_degrees_per_minute)
+    minute_angle = (minutes * (360 / 60))
+
+
+    angle_between = (hour_angle - minute_angle).abs
+
+    if angle_between > 180
+        angle_between = 360 - angle_between
+    end
+    angle_between
 end
+
+#
